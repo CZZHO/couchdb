803ee5e5857202fadbe048683b6b7ad7fe740695
czzho <70568760+CZZHO@users.noreply.github.com>
2021-05-26 00:11:57 +0800

    couchdb

--- Harvest/mapandreduce/datemapfunction.js
+++ Harvest/mapandreduce/datemapfunction.js
@@ -1,19 +1,3 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2513
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Bold;\f1\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red13\green95\blue24;\red234\green255\blue193;\red49\green49\blue49;
-\red83\green83\blue83;}
-{\*\expandedcolortbl;;\cssrgb\c0\c43922\c12549;\cssrgb\c93333\c100000\c80000;\cssrgb\c25098\c25098\c25098;
-\cssrgb\c40000\c40000\c40000;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\b\fs24 \cf2 \cb3 \expnd0\expndtw0\kerning0
-function
-\f1\b0 \cf4 (doc) \{\
-    
-\f0\b \cf2 if
-\f1\b0 \cf4 (doc.time \cf5 &&\cf4  doc.text) \{\
-        emit(doc.time, doc.text);\
-    \}\
-\}}
+function(doc) { if(doc.date && doc.text) 
+    { var created = new Date(doc.date);
+        emit([created.getFullYear(),created.getMonth()+1,created.getDate()],doc.text); }}
